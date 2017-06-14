e4cbae8c86455355bf238c19886e1e164de5457f
Dao Hai <daohai39@gmail.com>
2017-06-14 10:10:46 +0700

    Added Error Handle

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -3,4 +3,17 @@ declare(strict_types = 1);
 namespace Example;
 require __DIR__ . '/../vendor/autoload.php';
 
-echo 'Hello World';
+error_reporting(E_ALL);
+
+$enviroment = 'development';
+
+$whoops = new \Whoops\Run;
+if ($enviroment !== 'production'):
+	$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+else:
+	$whoops->pushHandler(function($e){
+		echo 'Todo: Friendly error page and send email to the developer';
+	});
+endif;
+
+$whoops->register();
