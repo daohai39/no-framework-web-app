e93bb5660681314535d8a0b5561438770a644d13
Dao Hai <daohai39@gmail.com>
2017-06-14 13:17:09 +0700

    Finish Dynamic Page

---
+++ src/Controllers/Page.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types = 1);
+namespace Example\Controllers;
+use Http\Response;
+use Example\Template\Renderer;
+use Example\Page\PageReader;
+
+class Page
+{
+	private $response;
+	private $renderer;
+	private $pageReader;
+
+	public function __construct(Response $response, Renderer $renderer, PageReader $pageReader)
+	{
+		$this->response = $response;
+		$this->renderer = $renderer;
+		$this->pageReader = $pageReader;
+	}
+
+	public function show($params)
+	{
+		$slug = $params['slug'];
+		$data['content'] = $this->pageReader->readBySlug($slug);
+		$html = $this->renderer->render('Page',$data);
+		$this->response->setContent($html);
+	}
+}
